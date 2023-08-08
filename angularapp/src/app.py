c0a23ccc61c0b22b6f5fd9ca3430f069ce0319f3
demo-college <d15784889@gmail.com>
2023-08-08 12:58:57 +0000

    ms

---
+++ angularapp/src/app.py
@@ -0,0 +1,87 @@
+print("Hiii");
+
+for i in range(1000):
+    print(i)
+
+for i in range(1000):
+    print(i)
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
+
+
+for i in range(1000):
+    print(i)
